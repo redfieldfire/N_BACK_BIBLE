e9e272e0e1c67129e221db27448ad80490534f1f
Christopher Serna Flores <cserna@controlla.com.mx>
2024-05-15 15:09:04 -0600

    start

---
+++ src/views/MainView.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const bibleBooks = [
+  //OLD TESTAMENT
+  "Génesis",
+  "Éxodo",
+  "Levítico",
+  "Números",
+  "Deuteronomio",
+  "Josué",
+  "Jueces",
+  "Rut",
+  "1 Samuel",
+  "2 Samuel",
+  "1 Reyes",
+  "2 Reyes",
+  "1 Crónicas",
+  "2 Crónicas",
+  "Esdras",
+  "Nehemías",
+  "Ester",
+  "Job",
+  "Salmos",
+  "Proverbios",
+  "Eclesiastés",
+  "Cantares",
+  "Isaías",
+  "Jeremías",
+  "Lamentaciones",
+  "Ezequiel",
+  "Daniel",
+  "Oseas",
+  "Joel",
+  "Amós",
+  "Abdías",
+  "Jonás",
+  "Miqueas",
+  "Nahum",
+  "Habacuc",
+  "Sofonías",
+  "Hageo",
+  "Zacarías",
+  "Malaquías",
+  
+  //NEW TESTAMENT
+  "Mateo",
+  "Marcos",
+  "Lucas",
+  "Juan",
+  "Hechos",
+  "Romanos",
+  "1 Corintios",
+  "2 Corintios",
+  "Gálatas",
+  "Efesios",
+  "Filipenses",
+  "Colosenses",
+  "1 Tesalonicenses",
+  "2 Tesalonicenses",
+  "1 Timoteo",
+  "2 Timoteo",
+  "Tito",
+  "Filemón",
+  "Hebreos",
+  "Santiago",
+  "1 Pedro",
+  "2 Pedro",
+  "1 Juan",
+  "2 Juan",
+  "3 Juan",
+  "Judas",
+  "Apocalipsis"
+];
+
+</script>
+
+<template>
+    <label> </label>
+</template>
+
+<style scoped>
+
+</style>
