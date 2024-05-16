42da9489da33fda257022203e2838279b6d11310
Christopher Serna Flores <cserna@controlla.com.mx>
2024-05-16 11:38:03 -0600

    colors and logic

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,8 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 
-const count = ref(0)
-const book = ref("")
+const count = ref(2)
+const currentBook = ref("")
+const pastBooks = ref([])
+const answerPastBook = ref("")
+const timeToAnswer = ref(false)
+
+const answer1 = ref("")
+const answer2 = ref("")
+
+const color1 = ref("")
+const color2 = ref("")
 
 const bibleBooks = [
   //OLD TESTAMENT
@@ -77,17 +86,83 @@ const bibleBooks = [
 ];
 
 const getRandomBook = () => {
-    const randomNumber = Math.floor(Math.random() * bibleBooks.length)
-    book.value = bibleBooks[randomNumber]
+    currentBook.value = randomBook()
+}
+
+const randomBook = (except = -1) => {
+    var randomNumber = Math.floor(Math.random() * bibleBooks.length)
+    while (except == randomNumber) {
+        randomNumber = Math.floor(Math.random() * bibleBooks.length)
+    }
+    return bibleBooks[randomNumber]
+}
+
+const addRandomBook = () => {
+    pastBooks.value.push(currentBook.value)
+}
+
+const addAnswerBook = () => {
+    answerPastBook.value = pastBooks.value.shift()
+}
+
+const setTimeToAnswer = (value) => {
+    timeToAnswer.value = value
+}
+
+const nextAction = () => {
+    getRandomBook()
+    addRandomBook()
+    if (pastBooks.value.length > count.value) {
+        setTimeToAnswer(true)
+        addAnswerBook()
+        setAnswers()
+    }
+}
+
+const setAnswers = () => {
+    if (Math.random() * 10 > 5) {
+        answer1.value = answerPastBook.value
+        answer2.value = randomBook(pastBooks.value.indexOf(answerPastBook.value))
+    } else {
+        answer1.value = randomBook(pastBooks.value.indexOf(answerPastBook.value))
+        answer2.value = answerPastBook.value
+    }
+}
+
+const guessAction = (value, button) => {
+    if(answerPastBook.value == value){
+        getRandomBook()
+        addRandomBook()
+        addAnswerBook()
+        setAnswers()
+
+        color1.value = ""
+        color2.value = ""
+    }
+    else {
+       if (button == 1) {
+        color1.value = "red"
+        color2.value = "green"
+       }
+       else {
+        color2.value = "red"
+        color1.value = "green"
+       }
+    }
 }
 
 </script>
 
 <template>
-    <div class="center">
-        <label>{{book}}</label>
-        </br>
-        <button  @click="getRandomBook">CLICK ME</button>
+    <div class="center column gap-30">
+        <label class="actual-book-title">{{currentBook}}</label>
+        <button class="button-next" v-if="!timeToAnswer" @click="nextAction">Siguiente!</button>
+        <div v-else class="center row gap-30">
+            <button class="button-answer" :style="{backgroundColor: color1}" @click="guessAction(answer1, 1)">{{answer1}}</button>
+            <button class="button-answer" :style="{backgroundColor: color2}" @click="guessAction(answer2, 2)">{{answer2}}</button>
+        </div>
+        <label hidden="false" v-for="(book, index) in pastBooks" :key="index">{{( index + 1 ) + " -> " + book}}</label>
+        <h2 hidden="false" >Answer: {{answerPastBook}}</h2>
     </div>
 </template>
 
@@ -96,6 +171,29 @@ const getRandomBook = () => {
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+
+    .column {
         flex-direction: column;
+    }
+    
+    .row {
+        flex-direction: row;
+    }
+
+    .gap-30 {
+        gap: 30px;
+    }
+
+    .actual-book-title {
+        font-size: 10rem;
+    }
+
+    .button-answer {
+        font-size: 3rem;
+    }
+
+    .button-next {
+        font-size: 3rem;
     }
 </style>
